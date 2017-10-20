678699a340c74a821539fb537aa168783e3f54b2
Dan Dorman <dan.dorman@gmail.com>
2017-10-20 09:45:27 -0600

    Update to release Workflowy version

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,13 +1,13 @@
 cask 'workflowy' do
-  version '0.0.1'
-  sha256 '9fa8ff9da4bec28076f524f2b52a47f77653ef243030fc6fed3836a1cc46fdb8'
+  version '0.1.8'
+  sha256 '46dac2678b55e4711bd46e6aab6e5ba1b2c49797d5e8f4a091d28a83b4650ac8'
 
   # github.com/workflowy/desktop was verified as official when first introduced to the cask
-  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy-#{version}.dmg"
+  url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
   appcast 'https://github.com/workflowy/desktop/releases.atom',
-          checkpoint: '58d829d6e781806eb2f928344cc60567b5c054f78e4381610b38ee422752bd4a'
+          checkpoint: 'cb2315841a1f0912b22e7337bf3b86667d812c6980cf58c1c028a20cd83e971a'
   name 'WorkFlowy'
-  homepage 'https://blog.workflowy.com/2017/04/18/preview-the-workflowy-desktop-app/'
+  homepage 'https://workflowy.com/downloads/mac/'
 
   app 'WorkFlowy.app'
 end
